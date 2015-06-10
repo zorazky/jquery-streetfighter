d9e988e66ce4c5291b9fad52244e755beaccb87c
zorazky <oswaldo.vazquez@gmail.com>
2015-06-10 18:03:31 -0500

    js first test

---
+++ js/app.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function(){
+		$('.ryu-still').show();
+		$('.ryu-ready').hide();
+	})
+	.mousedown(function() {
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show()
+		.animate(
+			{'left': '1020px'},
+			500,
+			function(){
+				$(this).hide();
+				$(this).css('left','520px');
+			}
+			);
+
+		//show hadouken and animate it to the right of the screen
+	})
+	.mouseup(function(){
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+		//ryu goes back to his ready position
+	});
+
+});
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
